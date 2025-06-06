65f8dbecae277323dd630bb62ca4565bbb00ee3d
Kirill <kirill.trapeznikov@str.us>
2025-06-05 21:27:40 -0400

    run debug.sh

--- run_debug.sh
+++ run_debug.sh
@@ -1,7 +1,11 @@
 export HF_TOKEN=$(cat ~/.cache/huggingface/token)
-docker build -t safe-test .
-docker run --gpus all \
+export LOCAL_CACHE=/disk1/kirill/tmp
+docker run -it --rm --gpus all \
   -e SAFE_DATASET_REPO=dsf-sandbox/video-challenge-debug \
-  -e MODEL_REPO=safe-challenge/safe-example-submission \
-  -e HF_TOKEN="$HF_TOKEN" \
-  safe-test
+  -e MODEL_REPO=dsf-sandbox/safe-video-example-submission \
+  -e HF_TOKEN=$HF_TOKEN \
+  -e PYTHONUNBUFFERED=1 \
+  -w /app/debug \
+  -v $LOCAL_CACHE:/tmp \
+  ghcr.io/stresearch/competitions:latest \
+  bash debug.sh
